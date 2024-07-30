f0969808f5aea5d44b2ae6838687227ff6b936a5
Евгений <listopadov_94@mail.ru>
2024-07-30 15:08:02 +0300

    отключения джостика если едитор

--- Assets/Screpts/Bootstrapper.cs
+++ Assets/Screpts/Bootstrapper.cs
@@ -1,27 +1,45 @@
 using Screpts.Services.Input;
+using SimpleInputNamespace;
 using UnityEngine;
 
 namespace Screpts
 {
     public class Bootstrapper : MonoBehaviour
     {
+        [SerializeField] private GameObject _joystick;
         public static IInputServices InputServices;
+        private bool _isMobile = false;
 
         public Bootstrapper()
         {
             RegisterInputService();
         }
 
-        private static void RegisterInputService()
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+            Joustick();
+        }
+
+        private void RegisterInputService()
         {
             if (Application.isEditor)
             {
                 InputServices = new StandaloneInputService();
+                _isMobile = false;
+                
             }
             else
             {
                 InputServices = new MobileInputService();
+                _isMobile = true;
             }
+        }
+
+        private void Joustick()
+        {
+            if (_isMobile == false)
+                _joystick.gameObject.SetActive(false);
         }
     }
 }
